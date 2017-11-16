d7a499a647953fbe17845877b873d86c99045067
Jerome Briot <jbtechlab@gmail.com>
2017-11-16 13:08:28 +0100

    Fix error in status LEDs color for door error and RFID frequency error

--- fw02_1/src/driver/led_status.c
+++ fw02_1/src/driver/led_status.c
@@ -106,8 +106,8 @@ void setLedsStatusColor( LED_STATUS status )
         case LEDS_ERROR_RFID_FREQ:
             LED_STATUS_R_SetLow( );
             LED_STATUS_G_SetHigh( );
-            LED_STATUS_B_SetHigh( );
-            LED_STATUS_Y_SetHigh( );
+            LED_STATUS_B_SetLow( );
+            LED_STATUS_Y_SetLow( );
             break;
 
     }
